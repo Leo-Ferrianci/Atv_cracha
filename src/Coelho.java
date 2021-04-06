7a50a676bd432f2dcc5eacd924982fed526fc3c8
Leo Ferrianci <58373615+Leo-Ferrianci@users.noreply.github.com>
2021-04-06 11:49:28 -0300

    Atv List, map e list

--- src/Coelho.java
+++ src/Coelho.java
@@ -8,12 +8,12 @@ public class Coelho {
 	float altura;
 	float massa;
 	String nome;
-	String raça;
+	String raï¿½a;
 	String cor;
 	
 	public Coelho(String n)
 	{
-		raça=n;
+		raï¿½a=n;
 	}
 	public void saltitar(int pulou) {System.out.println("O coelho pulou "+ pulou+ " vezes"); }
 	
@@ -27,44 +27,44 @@ public class Coelho {
 	
 	public static void main(String[] args) {
 
-        String raça1 = "Neatherland Dwarf";
+        String raï¿½a1 = "Neatherland Dwarf";
         String cor1 = "marrom";
-        String nome1 = "Sansão";
+        String nome1 = "Sansï¿½o";
 
         ArrayList<String> coelho1 = new ArrayList<>();
-        coelho1.add(raça1);
+        coelho1.add(raï¿½a1);
         coelho1.add(cor1);
         coelho1.add(nome1);        
 
-        System.out.println("Os detalhes do coelho 1 são: " + coelho1);
+        System.out.println("Os detalhes do coelho 1 sï¿½o: " + coelho1);
 		Coelho o1=new Coelho ("Neatherland Dwarf");
 		o1.saltitar(5);
         
-        String raça2 = "Florida White";
+        String raï¿½a2 = "Florida White";
         String cor2 = "Branco";
         String nome2 = "Tambor";
 
         ArrayList<String> coelho2 = new ArrayList<>();
-        coelho2.add(raça2);
+        coelho2.add(raï¿½a2);
         coelho2.add(cor2);
         coelho2.add(nome2);        
 
-        System.out.println("Os detalhes do coelho 2 são: " + coelho2);
+        System.out.println("Os detalhes do coelho 2 sï¿½o: " + coelho2);
         Coelho o2=new Coelho ("Florida White");
 		o2.comer("cenoura");
         
-        String raça3 = "Flemish Giant";
+        String raï¿½a3 = "Flemish Giant";
         String cor3 = "cinza";
         String nome3 = "Pernalonga";
 
         ArrayList<String> coelho3 = new ArrayList<>();
-        coelho3.add(raça3);
+        coelho3.add(raï¿½a3);
         coelho3.add(cor3);
         coelho3.add(nome3);        
 
-        System.out.println("Os detalhes do coelho 3 são: " + coelho3);
+        System.out.println("Os detalhes do coelho 3 sï¿½o: " + coelho3);
         Coelho o3=new Coelho ("Flemish Giant");
-		o3.chiar("O que há, velhinho?");
+		o3.chiar("O que hï¿½, velhinho?");
 		
 		Set<String> hash_Set = new HashSet<String>();
 		
@@ -87,6 +87,7 @@ public class Coelho {
 	        Map.Entry entry=(Map.Entry)itr.next();  
 	        System.out.println(entry.getKey()+" "+entry.getValue());  
 	    } 
+		
     }
 	
 	
